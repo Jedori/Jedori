a2517c5fa5ea275385115ba7d955346965935b16
Junseo Rho <joonseor@gmail.com>
2025-05-13 22:46:00 +0900

    Screenshot 사운드 추가

--- Assets/Script/Tool/ScreenshotHandler.cs
+++ Assets/Script/Tool/ScreenshotHandler.cs
@@ -1,69 +1,67 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 using System.Collections;
 using System.IO;
 using TMPro;
 
 public class ScreenshotHandler : MonoBehaviour
 {
-    public GameObject notificationPanel; 
-    public TMP_Text notificationText;  
+    public string screenshotBaseName = "Artwork";
+    public string screenshotExtension = ".png";
+    
+    public AudioSource audioSource;
+    public AudioClip shutterSound;
 
-    private string screenshotBaseName = "Artwork";
-    private string screenshotExtension = ".png";
+    public GameObject notificationPanel;
+    public TMP_Text notificationText;
+
     private string lastSavedPath = "";
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            TakeScreenshot();
+            StartCoroutine(TakeScreenshot());
         }
     }
 
-    void TakeScreenshot()
+    IEnumerator TakeScreenshot()
     {
         string folderPath = Application.dataPath + "/Resources/";
-
         if (!Directory.Exists(folderPath))
             Directory.CreateDirectory(folderPath);
 
         string filePath = folderPath + screenshotBaseName + screenshotExtension;
         int count = 1;
-
         while (File.Exists(filePath))
         {
             filePath = folderPath + screenshotBaseName + "_" + count + screenshotExtension;
             count++;
         }
-
         lastSavedPath = filePath;
+
+        audioSource.PlayOneShot(shutterSound);
 
         ScreenCapture.CaptureScreenshot(filePath);
         ShowNotification("Generating Artwork...");
 
-        StartCoroutine(PostScreenshotProcess());
-    }
-
-    IEnumerator PostScreenshotProcess()
-    {
         yield return new WaitForSeconds(1.0f);
-
         if (File.Exists(lastSavedPath))
-        {
             ShowNotification("Artwork saved successfully!");
-        }
         else
-        {
             ShowNotification("Artwork save failed...");
-        }
     }
+
 
     void ShowNotification(string message)
     {
-        notificationText.text = message;
-        notificationPanel.SetActive(true);
-        StartCoroutine(HideNotificationAfterSeconds(2.0f));
+        if (notificationPanel && notificationText)
+        {
+            notificationText.text = message;
+            notificationPanel.SetActive(true);
+            StartCoroutine(HideNotificationAfterSeconds(2.0f));
+        }
     }
 
     IEnumerator HideNotificationAfterSeconds(float seconds)
