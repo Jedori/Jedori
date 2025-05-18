648c608e73ef24b6ded109c0c9aaf45ba09ed4e0
YongHwan Song <hysong06@gmail.com>
2025-05-18 21:56:30 +0900

    StarSpawner.cs 업데이트

--- Assets/Script/Stellar/Initiate/StarSpawner.cs
+++ Assets/Script/Stellar/Initiate/StarSpawner.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 /// <summary>
 /// 힙 번호로 별을 생성하고 별자리 선을 그리는 스크립트입니다.
 /// 별 데이터는 JSON 파일에서 로드됩니다.
 /// </summary>
 public class StarSpawner : MonoBehaviour
-{   
+{
     [Header("Star Data")]
     [SerializeField] TextAsset lineJsonFile;
     [Tooltip("별 데이터가 존재하는 JSonFile입니다.")]
@@ -51,6 +52,112 @@ public class StarSpawner : MonoBehaviour
     [SerializeField] float timeZone = 9f;  // 한국 시간대
     [Tooltip("시간대 (UTC 기준)")]
 
+
+    private Dictionary<char, Dictionary<int,List<float>>> sptToRgb = new Dictionary<char, Dictionary<int,List<float>>>
+    {
+        {   
+            'M', new Dictionary<int,List<float>> {
+                    { 19, new List<float> { 1.0f, 0.491f, 0.144f } },
+                    { 18, new List<float> { 1.0f, 0.518f, 0.179f } },
+                    { 16, new List<float> { 1.0f, 0.542f, 0.202f } },
+                    { 15, new List<float> { 1.0f, 0.607f, 0.255f } },
+                    { 13, new List<float> { 1.0f, 0.648f, 0.286f } },
+                    { 12, new List<float> { 1.0f, 0.644f, 0.285f } },
+                    { 11, new List<float> { 1.0f, 0.641f, 0.289f } },
+                    { 9, new List<float> { 1.0f, 0.638f, 0.293f } },
+                    { 8, new List<float> { 1.0f, 0.638f, 0.3f } },
+                    { 7, new List<float> { 1.0f, 0.638f, 0.308f } },
+                    { 6, new List<float> { 1.0f, 0.638f, 0.315f } },
+                    { 5, new List<float> { 1.0f, 0.637f, 0.322f } },
+                    { 4, new List<float> { 1.0f, 0.635f, 0.327f } },
+                    { 2, new List<float> { 1.0f, 0.637f, 0.34f } },
+                    { 1, new List<float> { 1.0f, 0.635f, 0.346f } },
+                    { 0, new List<float> { 1.0f, 0.636f, 0.354f } },
+                }
+        },
+        {   
+            'K', new Dictionary<int,List<float>> {
+                    { 16, new List<float> { 1.0f, 0.641f, 0.369f } },
+                    { 14, new List<float> { 1.0f, 0.65f, 0.389f } },
+                    { 13, new List<float> { 1.0f, 0.662f, 0.411f } },
+                    { 11, new List<float> { 1.0f, 0.677f, 0.439f } },
+                    { 10, new List<float> { 1.0f, 0.696f, 0.47f } },
+                    { 9, new List<float> { 1.0f, 0.717f, 0.501f } },
+                    { 8, new List<float> { 1.0f, 0.739f, 0.533f } },
+                    { 7, new List<float> { 1.0f, 0.761f, 0.565f } },
+                    { 6, new List<float> { 1.0f, 0.781f, 0.595f } },
+                    { 5, new List<float> { 1.0f, 0.821f, 0.657f } },
+                    { 3, new List<float> { 1.0f, 0.84f, 0.691f } },
+                    { 2, new List<float> { 1.0f, 0.857f, 0.722f } },
+                    { 0, new List<float> { 1.0f, 0.872f, 0.753f } },
+                }
+        },
+        {
+            'G', new Dictionary<int,List<float>> {
+                    { 18, new List<float> { 1.0f, 0.886f, 0.783f } },
+                    { 16, new List<float> { 1.0f, 0.898f, 0.813f } },
+                    { 12, new List<float> { 1.0f, 0.91f, 0.845f } },
+                    { 8, new List<float> { 1.0f, 0.922f, 0.878f } },
+                    { 4, new List<float> { 1.0f, 0.931f, 0.905f } },
+                    { 2, new List<float> { 1.0f, 0.94f, 0.931f } },
+                }
+        },
+        {
+            'F', new Dictionary<int,List<float>> {
+                    { 19, new List<float> { 1.0f, 0.951f, 0.967f } },
+                    { 18, new List<float> { 1.0f, 0.96f, 0.998f } },
+                    { 16, new List<float> { 0.955f, 0.931f, 1.0f } },
+                    { 12, new List<float> { 0.922f, 0.908f, 1.0f } },
+                    { 10, new List<float> { 0.869f, 0.871f, 1.0f } },
+                    { 8, new List<float> { 0.844f, 0.855f, 1.0f } },
+                    { 6, new List<float> { 0.823f, 0.84f, 1.0f } },
+                    { 4, new List<float> { 0.782f, 0.812f, 1.0f } },
+                    { 2, new List<float> { 0.763f, 0.799f, 1.0f } },
+                    { 0, new List<float> { 0.725f, 0.773f, 1.0f } },
+                }
+        },
+        {   
+            'A', new Dictionary<int,List<float>> {
+                    { 18, new List<float> { 0.692f, 0.75f, 1.0f } },
+                    { 16, new List<float> { 0.674f, 0.738f, 1.0f } },
+                    { 14, new List<float> { 0.636f, 0.712f, 1.0f } },
+                    { 12, new List<float> { 0.606f, 0.69f, 1.0f } },
+                    { 8, new List<float> { 0.556f, 0.652f, 1.0f } },
+                    { 6, new List<float> { 0.546f, 0.645f, 1.0f } },
+                    { 4, new List<float> { 0.531f, 0.634f, 1.0f } },
+                    { 2, new List<float> { 0.508f, 0.616f, 1.0f } },
+                    { 0, new List<float> { 0.483f, 0.595f, 1.0f } },
+                }
+        },
+        {   
+            'B', new Dictionary<int,List<float>> {
+                    { 19, new List<float> { 0.472f, 0.586f, 1.0f } },
+                    { 18, new List<float> { 0.456f, 0.572f, 1.0f } },
+                    { 16, new List<float> { 0.446f, 0.562f, 1.0f } },
+                    { 14, new List<float> { 0.429f, 0.547f, 1.0f } },
+                    { 12, new List<float> { 0.421f, 0.541f, 1.0f } },
+                    { 10, new List<float> { 0.414f, 0.536f, 1.0f } },
+                    { 6, new List<float> { 0.408f, 0.532f, 1.0f } },
+                    { 5, new List<float> { 0.398f, 0.524f, 1.0f } },
+                    { 4, new List<float> { 0.39f, 0.517f, 1.0f } },
+                    { 3, new List<float> { 0.381f, 0.509f, 1.0f } },
+                    { 2, new List<float> { 0.373f, 0.503f, 1.0f } },
+                    { 1, new List<float> { 0.368f, 0.498f, 1.0f } },
+                }
+        },
+        {   
+            'O', new Dictionary<int,List<float>> {
+                    { 19, new List<float> { 0.361f, 0.491f, 1.0f } },
+                    { 16, new List<float> { 0.357f, 0.487f, 1.0f } },
+                    { 12, new List<float> { 0.359f, 0.487f, 1.0f } },
+                    { 10, new List<float> { 0.358f, 0.486f, 1.0f } },
+                    { 8, new List<float> { 0.357f, 0.485f, 1.0f } },
+                    { 6, new List<float> { 0.357f, 0.485f, 1.0f } },
+                    { 4, new List<float> { 0.359f, 0.487f, 1.0f } },
+                    { 2, new List<float> { 0.361f, 0.489f, 1.0f } },
+                }
+        },
+    };
     private Dictionary<int, GameObject> hipToStar = new();
     private Dictionary<int, StarData> starDataDict = new();
     private List<GameObject> constellationLines = new();  // 별자리 선 오브젝트 저장용
@@ -76,7 +183,7 @@ public class StarSpawner : MonoBehaviour
 
         // 시간이 지남에 따라 별의 위치 업데이트
         UpdateStarPositions();
-        
+
         // 별자리 선 업데이트
         UpdateConstellationLines();
     }
@@ -129,16 +236,16 @@ public class StarSpawner : MonoBehaviour
         // 그리니치 항성시(GST) 계산
         float t = (julianDate - 2451545.0f) / 36525.0f;
         float gst = 100.46061837f + 36000.770053608f * t + 0.000387933f * t * t - t * t * t / 38710000f;
-        
+
         // 관측자의 항성시(LST) 계산
         float lst = gst + observerLongitude;
-        
+
         // 시각각 계산
         float hourAngle = lst - ra;
-        
+
         // 0-360 범위로 정규화
         hourAngle = (hourAngle + 360f) % 360f;
-        
+
         return hourAngle;
     }
 
@@ -165,7 +272,7 @@ public class StarSpawner : MonoBehaviour
 
         // 시각각 계산
         float hourAngle = CalculateHourAngle(starData.ra);
-        
+
         // 고도와 방위각 계산
         float altitude = CalculateAltitude(starData.dec, hourAngle);
         float azimuth = CalculateAzimuth(starData.dec, hourAngle);
@@ -179,7 +286,7 @@ public class StarSpawner : MonoBehaviour
         // 고도 계산 공식
         float sinAlt = Mathf.Sin(dec * Mathf.Deg2Rad) * Mathf.Sin(observerLatitude * Mathf.Deg2Rad) +
                       Mathf.Cos(dec * Mathf.Deg2Rad) * Mathf.Cos(observerLatitude * Mathf.Deg2Rad) * Mathf.Cos(hourAngle * Mathf.Deg2Rad);
-        
+
         return Mathf.Asin(sinAlt) * Mathf.Rad2Deg;
     }
 
@@ -188,12 +295,12 @@ public class StarSpawner : MonoBehaviour
         // 방위각 계산 공식
         float cosAz = (Mathf.Sin(dec * Mathf.Deg2Rad) - Mathf.Sin(observerLatitude * Mathf.Deg2Rad) * Mathf.Sin(CalculateAltitude(dec, hourAngle) * Mathf.Deg2Rad)) /
                      (Mathf.Cos(observerLatitude * Mathf.Deg2Rad) * Mathf.Cos(CalculateAltitude(dec, hourAngle) * Mathf.Deg2Rad));
-        
+
         float sinAz = -Mathf.Sin(hourAngle * Mathf.Deg2Rad) * Mathf.Cos(dec * Mathf.Deg2Rad) /
                      Mathf.Cos(CalculateAltitude(dec, hourAngle) * Mathf.Deg2Rad);
-        
+
         float azimuth = Mathf.Atan2(sinAz, cosAz) * Mathf.Rad2Deg;
-        
+
         // 0-360 범위로 정규화
         return (azimuth + 360f) % 360f;
     }
@@ -212,6 +319,7 @@ public class StarSpawner : MonoBehaviour
 
         return new Vector3(x, y, z);
     }
+
 
     /// <summary>
     /// JSON 파일에서 별 데이터를 로드하고 별을 생성합니다.
@@ -264,11 +372,80 @@ public class StarSpawner : MonoBehaviour
         float scale = Mathf.Clamp(starScale / (starCopy.V + 2f), 0.1f, 2f);
         starObject.transform.localScale = Vector3.one * scale;
 
+        Color color = getColorFromSpectralTypes(star.sp_type);
+        Renderer renderer = starObject.GetComponent<Renderer>();
+        renderer.material.SetColor("_BaseColor", color);
+        renderer.material.SetColor("_CellColor", color);
+
+
+        /*
+        Add star instance to dictionary
+        */
         if (!hipToStar.ContainsKey(starCopy.hip))
         {
             hipToStar.Add(starCopy.hip, starObject);
         }
     }
+
+    Color getColorFromSpectralTypes(string sp_type)
+    {
+        if (string.IsNullOrWhiteSpace(sp_type) || !sptToRgb.ContainsKey(sp_type[0]))
+            sp_type = "G2V";  // default
+
+
+        int p = 0;
+
+        /*
+        분광형 클래스 계산
+        */
+        if (!sptToRgb.ContainsKey(sp_type[p])) p++;  // kA, kB, gG 등으로 시작하는 sp_type 처리
+        char sp_class = sp_type[p++];
+        if (!sptToRgb.ContainsKey(sp_class)) sp_type = "G2V";  // 7가지 분광형에 포함되지 않는 경우
+
+        /*
+        분광형 값 계산 -> 0.5단위 처리하기 위해 2배 곱한 값을 딕셔너리 key로 사용
+        */
+        int key = (sp_type[p++] - '0') * 2;
+        if (p < sp_type.Length && sp_type[p] == '.')
+            key += 1;
+
+        if (!sptToRgb[sp_class].ContainsKey(key))  // try to find the closest key
+        {
+            int a = key - 1;
+            int b = key + 1;
+
+            while (0 <= a || b <= 20)
+            {
+                if (sptToRgb[sp_class].ContainsKey(a)) {
+                    key = a;
+                    break;
+                }
+                if (sptToRgb[sp_class].ContainsKey(b)) {
+                    key = b;
+                    break;
+                }
+                a--; b++;
+            }
+        }
+        
+        /*
+        분광형에 따른 색상 반환 
+        */
+        var rgb = sptToRgb[sp_class][key];
+        Color result = new Color(rgb[0], rgb[1], rgb[2]);
+        return EnhanceContrast(result, 1.5f, 2.0f);
+    }
+
+    Color EnhanceContrast(Color color, float saturationBoost, float brightnessScale)
+    {
+        Color.RGBToHSV(color, out float h, out float s, out float v);
+
+        s = Mathf.Clamp01(s * saturationBoost);  // 채도 조절
+        v = Mathf.Clamp01(v * brightnessScale);  // 밝기 조절
+
+        return Color.HSVToRGB(h, s, v);
+    }
+
 
     void DrawConstellationLines()
     {
@@ -319,6 +496,7 @@ public class StarSpawner : MonoBehaviour
     {
         public LineGroup[] lines;
     }
+
 
     // 관측자 위치 설정 메서드
     public void SetObserverLocation(float latitude, float longitude, float altitude)
