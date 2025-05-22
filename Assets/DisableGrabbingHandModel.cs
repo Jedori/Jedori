ad3e255a93565be25827171eff78914c59a65478
AlgoUrUn <yls11091@gmail.com>
2025-05-22 13:11:19 +0900

    맵 업데이트

---
+++ Assets/DisableGrabbingHandModel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class DisableGrabbingHandModel : MonoBehaviour
+{
+    [SerializeField] private GameObject leftHandModel;
+    [SerializeField] private GameObject rightHandModel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+
+    }
+
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        Debug.Log("HideGrabbing");
+        if (args.interactorObject.transform.tag == "LeftHand")
+        {
+            Debug.Log("Left Hand HideGrabbing");
+            leftHandModel.SetActive(false);
+        }
+        if (args.interactorObject.transform.tag == "RightHand")
+        {
+            Debug.Log("Right Hand HideGrabbing");
+            rightHandModel.SetActive(false);
+        }
+    }
+
+    public void ShowGrabbingHand(SelectExitEventArgs args)
+    {
+        Debug.Log("ShowGrabbing");
+        if (args.interactorObject.transform.tag == "LeftHand")
+        {
+            Debug.Log("Left Hand ShowGrabbing");
+            leftHandModel.SetActive(true);
+        }
+        if (args.interactorObject.transform.tag == "RightHand")
+        {
+            Debug.Log("Right Hand ShowGrabbing");
+            rightHandModel.SetActive(true);
+        }
+    }
+}
