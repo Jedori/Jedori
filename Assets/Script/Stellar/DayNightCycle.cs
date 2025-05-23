d5f248d08bbae8c34fcf65f28e6453959ff0a051
YongHwan Song <hysong06@gmail.com>
2025-05-24 00:15:59 +0900

    DayNightCycle.cs

---
+++ Assets/Script/Stellar/DayNightCycle.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Light sun;
+
+    [Header("LightingPreset")]
+    [SerializeField] private Gradient skyColor;
+    [SerializeField] private Gradient equatorColor;
+    [SerializeField] private Gradient sunColor;
+
+    [Header("Settings")]
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+
+    private Material _skyboxMaterial;
+
+
+    private void Awake()
+    {
+        _skyboxMaterial = RenderSettings.skybox;
+        if (_skyboxMaterial == null)
+        {
+            Debug.LogError("스카이박스 매터리얼을 발견하지 못했습니다.");
+            return;
+        }
+
+        timeOfDay = 0f;  // CurrentTime과의 동기화를 위한 초기화 작업
+    }
+
+
+    private void Update()
+    {
+        timeOfDay = _skyboxMaterial.GetFloat("_CurrentTime") * 24;
+        if (timeOfDay > 24)
+            timeOfDay -= 24;
+
+        UpdateSunRotation();
+        UpdateLighting();
+    }
+
+
+    private void OnValidate()
+    {
+        UpdateSunRotation();
+        UpdateLighting();
+    }
+
+
+    private void UpdateSunRotation()
+    {
+        float sunRotation = Mathf.Lerp(-90, 270, timeOfDay / 24);
+        sun.transform.rotation = Quaternion.Euler(sunRotation, sun.transform.rotation.y, sun.transform.rotation.z);
+    }
+
+    private void UpdateLighting()
+    {
+        float timeFraction = timeOfDay / 24;
+        RenderSettings.ambientEquatorColor = equatorColor.Evaluate(timeFraction);
+        RenderSettings.ambientSkyColor = skyColor.Evaluate(timeFraction);
+        sun.color = sunColor.Evaluate(timeFraction);
+    }
+}
