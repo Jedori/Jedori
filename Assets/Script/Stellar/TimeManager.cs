f6d3d3e7212fddbb08053bef31a72e26f0ccd19c
rillbrill <yd3082929@naver.com>
2025-05-24 12:33:36 +0900

    Time Manager 코드 복원

--- Assets/Script/Stellar/TimeManager.cs
+++ Assets/Script/Stellar/TimeManager.cs
@@ -26,7 +26,6 @@ public class TimeManager : MonoBehaviour
     private float previousHour;
     private float previousMinute;
     private float previousSecond;
-    float UpdateController = 60;
 
     // 싱글톤 패턴
     public static TimeManager Instance { get; private set; }
@@ -59,32 +58,26 @@ public class TimeManager : MonoBehaviour
 
     private void Update()
     {
-         
-        UpdateController += Time.deltaTime;
-        second += Time.deltaTime * timeScale;
-        if (UpdateController > 60f)
+        // 시간 자동 진행
+        if (timeScale != 0)
         {
-            if (timeScale != 0)
-            {
-                UpdateTime();
-            }
-
-            // 시간이 변경되었는지 확인
-            if (HasTimeChanged())
-            {
-                // 시간이 변경되면 즉시 모든 천체 업데이트
-                ForceUpdateAllCelestialBodies();
-
-                // 현재 값 저장
-                SaveCurrentTimeValues();
-            }
-
-            // 시간 업데이트
-            UpdateDateTime();
-            CalculateJulianDate();
-            UpdateController = 0;
+            second += Time.deltaTime * timeScale;
+            UpdateTime();
         }
-        
+
+        // 시간이 변경되었는지 확인
+        if (HasTimeChanged())
+        {
+            // 시간이 변경되면 즉시 모든 천체 업데이트
+            ForceUpdateAllCelestialBodies();
+            
+            // 현재 값 저장
+            SaveCurrentTimeValues();
+        }
+
+        // 시간 업데이트
+        UpdateDateTime();
+        CalculateJulianDate();
     }
 
     private bool HasTimeChanged()
